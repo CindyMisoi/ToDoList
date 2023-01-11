5cb92aa82bf17a65a90f13a67b62dd4ead678ae9
CindyMisoi <crmisoi2k@gmail.com>
2023-01-11 10:22:39 +0300

    added TodoList component to App component

--- src/component/todoItem.js
+++ src/component/todoItem.js
@@ -4,6 +4,11 @@ function TodoItem(){
     return(
         <div>
             <h1>Todo Items</h1>
+            <ul>
+                <li>eat</li>
+                <li>code</li>
+                <li>sleep</li>
+            </ul>
         </div>
     )
 }
