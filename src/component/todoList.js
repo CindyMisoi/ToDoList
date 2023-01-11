3930a68751188441a559cc20703d15e022819f69
CindyMisoi <crmisoi2k@gmail.com>
2023-01-11 04:03:32 +0300

    exported todoItem component within the todoList component

--- src/component/todoList.js
+++ src/component/todoList.js
@@ -1,9 +1,11 @@
 import React from "react";
+import TodoItem from "./todoItem";
 
 function TodoList(){
     return(
         <div>
             <h1>Todo List</h1>
+            <TodoItem />
         </div>
     )
 }
